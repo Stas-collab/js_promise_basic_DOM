5425f9e5fa730101d32682c38c0342e5d62643eb
Stas <oleksii.stanislav@student.uzhnu.edu.ua>
2025-12-05 17:19:52 +0200

    Add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,60 @@
 'use strict';
 
 // write your code here
+const logo = document.querySelector('.logo');
+
+function promis1() {
+  return new Promise((resolve, reject) => {
+    logo.addEventListener('click', () => {
+      resolve();
+    });
+  });
+}
+
+function promis2() {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      reject(Error);
+    }, 3000);
+  });
+}
+
+async function promisOne() {
+  try {
+    await promis1();
+
+    const div = document.createElement('div');
+
+    div.classList.add('message');
+    div.textContent = 'Promise was resolved!';
+    document.body.appendChild(div);
+  } catch {
+    const div = document.createElement('div');
+
+    div.classList.add('message');
+    div.classList.add('error-message');
+    div.textContent = 'Promise was rejected!';
+    document.body.appendChild(div);
+  }
+}
+
+async function promisTwo() {
+  try {
+    await promis2();
+
+    const div = document.createElement('div');
+
+    div.classList.add('message');
+    div.textContent = 'Promise was resolved!';
+    document.body.appendChild(div);
+  } catch {
+    const div = document.createElement('div');
+
+    div.classList.add('message');
+    div.classList.add('error-message');
+    div.textContent = 'Promise was rejected!';
+    document.body.appendChild(div);
+  }
+}
+promisOne();
+promisTwo();
